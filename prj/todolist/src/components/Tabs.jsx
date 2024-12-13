7e66ec23bc44c81253dce37bcdbc65e073184215
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-13 22:47:47 +0700

    tabs

--- prj/todolist/src/components/Tabs.jsx
+++ prj/todolist/src/components/Tabs.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function Tabs() {
+  const tabs = ['All', 'Open', 'Completed']
     return (
       <nav>
-        <button></button>
+        {tabs.map((tab, tabIndex) => {
+          return (
+            // ensure each tab has a key
+            <button key={tabIndex}>
+              <h4>{tab}</h4>
+            </button>
+          )
+        })}
       </nav>
   )
 }
