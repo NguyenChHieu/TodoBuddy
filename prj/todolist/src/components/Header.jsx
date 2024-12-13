0970d1cdb5cafdda1b12ef824d4c828f2985c618
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-13 22:53:44 +0700

    tab

--- prj/todolist/src/components/Header.jsx
+++ prj/todolist/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-        <h1>You</h1>
+        <h1 className='text-gradient'>You</h1>
     </header>
   )
 }
--- prj/todolist/src/components/Tabs.jsx
+++ prj/todolist/src/components/Tabs.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default function Tabs() {
   const tabs = ['All', 'Open', 'Completed']
+    // mapping
     return (
       <nav>
         {tabs.map((tab, tabIndex) => {
           return (
             // ensure each tab has a key
-            <button key={tabIndex}>
-              <h4>{tab}</h4>
+            <button key={tabIndex}
+            className='tab-button'>
+              <h4>{tab} <span>(0)</span></h4>
             </button>
           )
         })}
