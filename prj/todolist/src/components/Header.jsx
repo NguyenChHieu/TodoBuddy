46cd52e058bca00720fecad9c48cedd4e0c217d6
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-13 23:38:05 +0700

    numoftasks

--- prj/todolist/src/components/Header.jsx
+++ prj/todolist/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
+  const {todos} = props
+  const todosLength = todos.length
+
+  const isTasksPlural = todosLength != 1
+  const tasksOrTasks = isTasksPlural ? 'tasks' : 'task'
+
   return (
     <header>
-        <h1 className='text-gradient'>You</h1>
+        <h1 className='text-gradient'>You have {todosLength} open {tasksOrTasks}.</h1>
     </header>
   )
 }
