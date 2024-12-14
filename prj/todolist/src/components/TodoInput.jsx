c78aeb747ade08afa8b2a906bdf37688f82b8ded
NguyenChHieu <nguyenchihieu178205@gmail.com>
2024-12-14 09:25:12 +0700

    add buttoon

--- prj/todolist/src/components/TodoInput.jsx
+++ prj/todolist/src/components/TodoInput.jsx
@@ -1,6 +1,12 @@
 export default function TodoInput() {
     return (
         <>
+        <div className="input-container">
+            <input placeholder="Add todo here"/>
+            <button>
+                <i class="fa-solid fa-plus"></i>
+            </button>
+        </div>
         </>
     )
 }
--- prj/todolist/src/components/TodoList.jsx
+++ prj/todolist/src/components/TodoList.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
+export default function TodoList(props) {
+  const { todos } = props
+  const tab = 'All'
+  const filterTodoList = tab === 'All' ? 
+      todos : tab === 'Completed' ?
+        todos.filter(val => val.complete) :
+        todos.filter(val => !val.complete)
+
   return (
-    <div><TodoCard/></div>
-  )
+    <>
+      {todos.map((todo, todoIndex) => {
+        return (
+          <TodoCard 
+            key={todoIndex} 
+            todoIndex={todoIndex} 
+            todo={todo}/>
+        )
+        
+        })}
+      
+    </>)
 }
